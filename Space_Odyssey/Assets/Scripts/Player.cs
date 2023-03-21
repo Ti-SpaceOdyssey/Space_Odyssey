329aae661d6098099ff6caae2874aab30c1b55a5
Fernanda Mendes <fernanda.gomes.1117337@sga.pucminas.br>
2023-03-21 18:07:18 -0300

    Movimentação Player

--- Space_Odyssey/Assets/Scripts/Player.cs
+++ Space_Odyssey/Assets/Scripts/Player.cs
@@ -1,18 +1,52 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+
+    [Header("Movimentacao")]
+    [Range(0f,2f)]
+    [SerializeField]
+    private float velocidadeMovimento = 1f;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        Movimentacao();
+
+        areaPlayer();
+
+
+    }
+
+    private void areaPlayer()
+    {
+
         
+
+    }
+
+    private void Movimentacao()
+    {
+        var x = Input.GetAxis("Horizontal");
+        var y = Input.GetAxis("Vertical");
+
+        var mover = new Vector3(
+            x * velocidadeMovimento * Time.deltaTime,
+            y * velocidadeMovimento * Time.deltaTime,
+            0f
+        );
+
+        transform.Translate(mover);
     }
 }
